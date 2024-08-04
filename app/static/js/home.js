31eacc268c9d1908b02f6f48777eb3b4caca7d85
elijah <elmmullens@gmail.com>
2024-08-04 12:42:16 +0800

    simple aside animated in project

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -1,11 +1,40 @@
 const docxExtension = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+let numFiles = 0;
+
+function openFileList() {
+    $("#file-list").css("width", "15%");
+    $("#drop-zone").css("width", "85%");
+    $("#file-list").prop('hidden', false);
+}
+
+function closeFileList() {
+    $("#file-list").css("width", "0%");
+    $("#drop-zone").css("width", "100%");
+    $("#file-list").prop('hidden', true);
+}
+
+function handleFileList(change) {
+    switch (change) {
+        case 1:
+            if (numFiles === 0) { openFileList(); }
+            break;
+        case -1:
+            if (numFiles === 1) { closeFileList(); }
+            break;
+    }
+}
 
 function handleFiles(files) {
     var $fileList = $('#file-list');
-    $fileList.empty();
     $.each(files, function(index, file) {
         if (file.type === docxExtension) {
+            if (numFiles === 2) {
+                alert("Maximum of 2 files")
+                return;
+            }
+            handleFileList(1);
             $fileList.append('<p>' + file.name + '</p>');
+            numFiles++;
         } else {
             alert('Only .docx files are allowed.');
         }
