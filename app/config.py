15ae54d9ebf11270cd1732f19990c604d05938fc
Lok Yx <lok@trilliongaming.com>
2024-08-05 16:03:36 +0800

    Implement file input handling, validation, flash messages

--- app/config.py
+++ app/config.py
@@ -2,4 +2,6 @@ import os
 
 
 class Config(object):
+    UPLOAD_EXTENSIONS = ['.docx']
     DEBUG = os.environ.get("DEBUG")
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'never-guess'
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,19 @@
-from flask import render_template
+from flask import render_template, flash
 from app import app
+from app.forms import MatchDocumentForm
 
 @app.context_processor
 def inject_global_variable():
     return dict(project_name="DocuMatcher")
 
 @app.route('/', methods=['GET'])
-@app.route('/home')
+@app.route('/home', methods= ['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    form = MatchDocumentForm()
+    if form.validate_on_submit():
+        print(form.files.data)
+        flash('Docx is sent to backend.', 'success')
+    return render_template('home.html', form=form)
 
 @app.route('/team', methods=['GET'])
 def team():
