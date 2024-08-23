9cf0a42c74a76f634754d7dc3525d77d486fa0eb
elijah <elmmullens@gmail.com>
2024-08-23 21:11:31 +0800

    docker building with error: OCI create failed 'entry.sh'

--- app/__init__.py
+++ app/__init__.py
@@ -11,10 +11,14 @@ def page_not_found(e):
     return render_template('/layout/page_not_found.html', project_name="DocuMatcher"), 404
 
 
-def create_app(config: Config):
+def create_app(config: Config | None):
+    if not config:
+        config = Config
+
     app = Flask(__name__)
     app.config.from_object(config)
     app.register_error_handler(404, page_not_found)
+
     return app
 
 
