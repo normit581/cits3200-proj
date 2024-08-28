c17ae3c8533c198118e420cd706467ee9962b081
aidan <aidenpower03@gmail.com>
2024-08-28 14:45:29 +0800

    Neatned up rsid_match, rebase

--- app/utilities/rsid.py
+++ app/utilities/rsid.py
@@ -1,6 +1,19 @@
 import zipfile
 import xml.etree.ElementTree as ET
-import re
+
+def clean_text(elem):
+
+    # Clean the text from XML
+    ns = {'ns0': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
+    texts = []
+
+    # find all with ns0 tag
+    for t in elem.findall('.//ns0:t', ns):
+        # if content not empty then append to list, make sure to clean text, strip
+        if t.text:
+            texts.append(t.text.strip())
+    # return all text in single string
+    return ' '.join(texts)
 
 # Function to extract RSID from a .docx file
 def rsid_extract(docx_path) :
@@ -14,18 +27,20 @@ def rsid_extract(docx_path) :
 
         # Parse the XML content
         root = ET.fromstring(document_xml)
-
+        
         # Find all elements with RSID attributes
         for elem in root.iter():
             for attrib in elem.attrib:
                 if attrib.startswith('{http://schemas.openxmlformats.org/wordprocessingml/2006/main}rsid'):
                     rsid = elem.attrib[attrib]
-                    
-                    if rsid in rsid_dict : 
+                    # clean text and add to location
+                    cleaned_text = clean_text(elem)
+                    if rsid in rsid_dict :
+                        # add count and add cleaned text to location of dict
                         rsid_dict[rsid]['count'] += 1
-                        rsid_dict[rsid]['location'].append(elem)
+                        rsid_dict[rsid]['location'].append(cleaned_text)
                     else :
-                        rsid_dict[rsid] = {'count': 1, 'location': [elem]}
+                        rsid_dict[rsid] = {'count': 1, 'location': [cleaned_text]}
                     rsid_count += 1
         # print(rsid_dict[rsid]['location'])
     return rsid_dict, rsid_count
@@ -35,23 +50,19 @@ def rsid_match2(rsid1, rsid2) :
     dict2, total2 = rsid2
     common_rsid_count = 0
     matching_rsid = {}
-   
-    for rsid in dict1 :
-        print('a')
-        if rsid in dict2 :
-            
+
+    for rsid in dict1:
+        if rsid in dict2:
             common_rsid_count += min(dict1[rsid]['count'], dict2[rsid]['count'])
-            
-            matching_rsid = {
+            # for each rsid, add count, and location of both doc rsid to dict
+            matching_rsid[rsid] = {
                 'count': min(dict1[rsid]['count'], dict2[rsid]['count']),
-                'location1': dict1[rsid]['location'],
-                'location2': dict2[rsid]['location']
+                'text1': dict1[rsid]['location'],
+                'text2': dict2[rsid]['location']
             
             }
-    print(matching_rsid['location1'][:50])
-    print("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
-    print(matching_rsid['location2'][:50])
+
     rsid_match = 100 * common_rsid_count / total1
-    
+    # print("common rsid:", common_rsid_count)
     return rsid_match, matching_rsid
 
