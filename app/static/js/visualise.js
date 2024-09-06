ceda7d80e24cf63075424e4dabfce75cbbac86df
Lok Yx <lok@trilliongaming.com>
2024-09-06 19:03:38 +0800

    remove setting bar, add context menu.
    - remove setting bar in visualise page
    - add custom context menu(right click) in visualise page for customisation output/format
    - change all onclick buttons from HTML to JQuery
    - add hide All button and hide Selector button colours
    - make the main content more larger to 'container-fluid'
    - improve jquery code
    - bg colour in pdf when window.print() not working bug fix

--- app/static/js/visualise.js
+++ app/static/js/visualise.js
@@ -1,6 +1,7 @@
-
-let currentFontSize = 16; // Default font size
+const defaultFontSize = 16; // Default font size
+let currentFontSize = defaultFontSize;
 let longPressInterval;
+const contextMenuID = 'custom-context-menu';
 
 function adjustCardHeight() {
     const height = document.getElementById('cardHeightInput').value;
@@ -10,17 +11,24 @@ function adjustCardHeight() {
     });
 }
 
-function rsidHideAll(btn) {
+function rsidColourToggleVisibility(btn, target, isAll = false, hideText = 'Hide', showText = 'Unhide') {
     const $btn = $(btn);
     const $icon = $btn.find('i');
-    if ($icon.hasClass('fa-eye-slash')){
-        $icon.removeClass('fa-eye-slash').addClass('fa-eye')
-        $btn.html($btn.html().replace('Hide All','Unhide All'))
-        document.querySelectorAll('.card-body p').forEach(p => p.style.display = 'none');
-    } else{
-        $icon.removeClass('fa-eye').addClass('fa-eye-slash')
-        $btn.html($btn.html().replace('Unhide All','Hide All'))
-        document.querySelectorAll('.card-body p').forEach(p => p.style.display = 'block');
+
+    const isHidden = $icon.hasClass('fa-eye-slash');
+    const action = isHidden ? 'addClass' : 'removeClass';
+    const reverse = isHidden ? 'removeClass' : 'addClass';
+    
+    $icon[action]('fa-eye')[reverse]('fa-eye-slash');
+    $btn.html($btn.html().replace(isHidden ? showText : hideText, isHidden ? hideText : showText));
+
+    $(target)[action]('hidden-colour');
+
+    if (isAll)
+        $btn.siblings('button.rsidButton').each(function() {
+            $(this).find('i')[action]('fa-eye')[reverse]('fa-eye-slash');
+            $(this).html($(this).html().replace(isHidden ? showText : hideText, isHidden ? hideText : showText));
+        });{
     }
 }
 
@@ -31,23 +39,9 @@ function switchVisualiseDocx(){
     $(firstDocx).insertAfter($(secondDocx));
 }
 
-function adjustFontSize() {
+function adjustFontSize(direction) {
+    currentFontSize = direction === 0 ? defaultFontSize : Math.max(10, Math.min(35, currentFontSize + direction));
     $('.card-body p').css('font-size', currentFontSize + 'px');
-}
-
-function decreaseFontSize() {
-    currentFontSize = Math.max(10, currentFontSize - 1); // min 10px
-    adjustFontSize();
-}
-
-function resetFontSize() {
-    currentFontSize = 16;
-    adjustFontSize();
-}
-
-function increaseFontSize() {
-    currentFontSize = Math.min(35, currentFontSize + 1); // max 35px
-    adjustFontSize();
 }
 
 function handleLongPress(action) {
@@ -58,45 +52,119 @@ function clearLongPress() {
     clearInterval(longPressInterval);
 }
 
-function toggleElementsVisibility(isVisible) {
-    const $navBar = $("nav");
-    const $settingBar = $("#setting-bar-container");
-    const $visualiseResult = $("#visualise-result-container");
-
+function pdfToggleElementsVisibility(isVisible) {
+    const $contextMenu = $(`#${contextMenuID}`);
     const action = isVisible ? 'show' : 'hide';
-    const containerClass = isVisible ? "container" : "container-fluid";
-
-    $navBar[action]();
-    $settingBar[action]();
-    $visualiseResult.attr("class", containerClass);
-    $visualiseResult.find(".card-body").toggleClass('pdf', !isVisible)
+    $contextMenu[action]();
 }
 
 function exportPDF() {
-    toggleElementsVisibility(false);
+    pdfToggleElementsVisibility(false);
     window.print();
-    toggleElementsVisibility(true);
+    pdfToggleElementsVisibility(true);
 }
 
 function exportSinglePDF() {
     exportPDF();
 }
 
-$(document).ready(function() {
-    $('#decrease-btn').on('mousedown', function() {
-        handleLongPress(decreaseFontSize);
-    }).on('mouseup mouseleave', clearLongPress);
+function showContextMenu(e){
+    const $contextMenu = $(`#${contextMenuID}`);
+    const mouseX = e.pageX;
+    const mouseY = e.pageY;
+    const contextMenuHeight = $contextMenu.outerHeight();
+    const contextMenuWidth = $contextMenu.outerWidth();
+    const windowHeight = window.innerHeight;
+    const windowWidth = window.innerWidth;
+    
+    const availableSpaceBelow = windowHeight - (mouseY - window.scrollY);
+    const availableSpaceRight = windowWidth - mouseX;
 
-    $('#increase-btn').on('mousedown', function() {
-        handleLongPress(increaseFontSize);
-    }).on('mouseup mouseleave', clearLongPress);
+    let topPosition = mouseY;
+    if (availableSpaceBelow < contextMenuHeight) {
+        topPosition = mouseY - contextMenuHeight;
+    }
+
+    let leftPosition = mouseX;
+    if (availableSpaceRight < contextMenuWidth) {
+        leftPosition = mouseX - contextMenuWidth;
+    }
+
+    $contextMenu
+        .css({ top: `${topPosition}px`, left: `${leftPosition}px` })
+        .show();
+}
+
+function configureContextMenuButtons(){
+    $('#pdf-btn').on('click', () => exportSinglePDF() );
+
+    $('#switch-card-btn').on('click', () => switchVisualiseDocx() );
+
+    $('#decrease-btn')
+        .on('click', () => adjustFontSize(-1))
+        .on('mousedown', () => handleLongPress(() => adjustFontSize(-1)))
+        .on('mouseup mouseleave', clearLongPress);
+
+    $('#reset-btn').on('click', () => adjustFontSize(0));
+
+    $('#increase-btn')
+        .on('click', () => adjustFontSize(1))
+        .on('mousedown', () => handleLongPress(() => adjustFontSize(1)))
+        .on('mouseup mouseleave', clearLongPress);
+
+    $('#adjust-height-btn #cardHeightInput')
+        .on('input', () => adjustCardHeight() );
+
+    $('#hide-all-colour-btn').on('click', function() { 
+        const $target = $('.card-body p');
+        rsidColourToggleVisibility(this, $target, isAll = true);
+    });
+}
+
+$(document).ready(function() {
+    $(`nav, #${contextMenuID}`).hide();
+
+    configureContextMenuButtons();
 
     $('p[data-colour]').each(function() {
         const colour = $(this).data('colour');
         const cssStyle = colour === 'red' ? 'color' : 'background-color';
         const cssColour = colour === 'red' ? 'red' : `light${colour}`;
         $(this).css(cssStyle, cssColour);
-        $(this).css('text-shadow', 'black 0px 0px 1px')
-        $(this).css('display', 'inline')
+    });
+
+    $(document).on('contextmenu', '.card-body > div', function (e) {
+        e.preventDefault();
+
+        const $divContainer = $(this);
+        const $btnGroup = $(`#${contextMenuID} .btn-group-vertical`);
+        $btnGroup.find('.rsidButton').remove();
+
+        $divContainer.find('p').each(function() {
+            const $pTarget = $(this);
+            const isVisible = $pTarget.hasClass('hidden-colour');
+            const buttonHTML = isVisible 
+                ? `<i class="fa-solid fa-eye ps-2"></i> Unhide Colour ${$pTarget.data('colour')}` 
+                : `<i class="fa-solid fa-eye-slash ps-2"></i> Hide Colour ${$pTarget.data('colour')}`;
+            
+            const $button = $('<button>', {
+                class: 'btn btn-light text-start rsidButton',
+                type: 'button',
+                html: buttonHTML
+            });
+
+            $button.click(function() {
+                rsidColourToggleVisibility(this, $pTarget);
+            });
+            $btnGroup.append($button);
+        });
+
+        showContextMenu(e);
+    });
+
+    $(document).on('click', function(e) {
+        if (!$(e.target).closest(`#${contextMenuID}`).length) {
+            $(`#${contextMenuID}`).hide();
+        }
     });
 });
