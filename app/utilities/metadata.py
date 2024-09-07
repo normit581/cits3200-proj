6d8d377b1fc45d4236a47c3b85455299e9897307
aidan <aidenpower03@gmail.com>
2024-09-07 16:28:05 +0800

    Testing but with ' ' in meta

--- app/utilities/metadata.py
+++ app/utilities/metadata.py
@@ -1,5 +1,5 @@
 from docx import Document
-
+import random
 def extract_metadata(docx):
     doc = Document(docx)
     # metadata for display
@@ -8,22 +8,45 @@ def extract_metadata(docx):
         'created': doc.core_properties.created,
         'paragraphs': [para.text for para in doc.paragraphs if para.text.strip()]
     }
-    
+
+    metadata["colours"] = [['red', 'blue', 'green', 'yellow', 'cyan', 'grey'][random.randint(0,5)] for i in range(-1, len(metadata["paragraphs"]))]
     return metadata
 
-def rsid_with_metadata(metadata, matching_rsid):
+
+def rsid_with_metadata(metadata, rsid1, rsid2, matching_rsid):
     # comparison dict with title and paragraphs[{rsid:, text:}]
     comparison_meta = {
         'title': metadata.get('title'),
         'paragraphs': []
     }
-    # for each rsid, and dict of matching rsid
-    for rsid, info in matching_rsid.items():
-        # for each text in the matching rsid append to paragraphs with rsid and text
-        for text in info['text1']:
-            comparison_meta['paragraphs'].append({
-                'rsid': rsid,
-                'text': text
-            })
+    # all rsid is combine 1 and 2
+    dict1, _ = rsid1
+    dict2, _ = rsid2
 
+    all_rsid = {**dict1, **dict2}
+    # print(all_rsid)
+    # colour for each rsid
+    rsid_colors = {rsid: 'red' if rsid in matching_rsid else 
+                   ['blue', 'green', 'yellow', 'cyan', 'grey'][random.randint(0, 4)] 
+                   for rsid in all_rsid}
+    
+    # tracking texts to avoid duplication
+    added_text = set()
+
+    # iterate through rsids
+    for rsid, info in all_rsid.items():
+        # get colour of current rsid
+        color = rsid_colors[rsid]
+        # for each rsid append text with corresponding color
+        for text in info['location']:
+            # skip empty or duplicates
+            if text.strip() and (rsid,text) not in added_text:
+                comparison_meta['paragraphs'].append({
+                    'rsid': rsid,
+                    'color': color,
+                    'text': text
+                })
+                # add to set
+                added_text.add((rsid,text))
+    print(comparison_meta)
     return comparison_meta
