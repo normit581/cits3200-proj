79a5d8f4732a5444584e591f560ddbbe43baac3e
Lu0922 <cklai0922@gmail.com>
2024-09-16 15:17:22 +0800

    adjust test_metadata.py to latest update from main

--- test/unittest/test_utilities/test_metadata.py
+++ test/unittest/test_utilities/test_metadata.py
@@ -15,11 +15,19 @@ def test_metadata_extract(setup_logger, setup_files) :
     for file in os.listdir(test_directory) :
         if file.endswith('docx') :
             file_path = os.path.join(test_directory, file)
-            metadata = extract_metadata(file_path)
+            rsid1 = rsid_extract(file_path)
+            rsid2 = rsid_extract(file_path)
+
+            similarity, matching_rsid = rsid_match2(rsid1, rsid2)
+            metadata1, metadata2 = extract_metadata(file_path, rsid1, rsid2, matching_rsid)
             
-            assert metadata['title'] is not None
-            assert metadata['created'] is not None
-            assert metadata['paragraphs'] is not None
+            assert metadata1['title'] is not None
+            assert metadata1['created'] is not None
+            assert metadata1['paragraphs'] is not None
             
-            test_log = f"{file} - metadata extracted - title: {metadata['title']} - created: {metadata['created']}"
+            assert metadata2['title'] is not None
+            assert metadata2['created'] is not None
+            assert metadata2['paragraphs'] is not None
+            
+            test_log = f"{file} - metadata extracted - title: {metadata1['title']} - created: {metadata1['created']}"
             logger.log(test_log, 'info')
