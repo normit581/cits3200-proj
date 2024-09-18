120dd9d08eb98f672a7a854b260efdc97edbc12a
lu0922 <23687543@student.uwa.edu.au>
2024-09-18 10:30:11 +0800

    fixed selenium get address

--- test/test_selenium.py
+++ test/test_selenium.py
@@ -26,8 +26,9 @@ class FlaskSeleniumTest(unittest.TestCase):
         cls.driver = webdriver.Chrome()
 
         # start test server
-        cls.app.testing = True
-        cls.app.test_client().get('/')
+        # cls.app.testing = True
+        # cls.app.test_client().get('/')
+        cls.driver.get('http://127.0.0.1:5000')         # we have to start the server manually for now
 
     @classmethod
     def tearDownClass(cls):
@@ -35,48 +36,50 @@ class FlaskSeleniumTest(unittest.TestCase):
         cls.app_context.pop()
 
     def setUp(self):
-        if not self.client:
-            self.skipTest('Web browser not available')
+        # if not self.client:
+        #     self.skipTest('Web browser not available')
+        pass
+    
     def tearDown(self):
         pass
     # tests
-    # def test_drag_and_drop_files(self):
-    #     # wait 5 secs for drop_area to be available
-    #     drop_area = WebDriverWait(self.driver, 3).until(
-    #         EC.element_to_be_clickable(By.CSS_SELECTOR, ".drop-zone") 
-    #     )
+    def test_drag_and_drop_files(self):
+        # wait 5 secs for drop_area to be available
+        drop_area = WebDriverWait(self.driver, 3).until(
+            EC.element_to_be_clickable(By.CSS_SELECTOR, ".drop-zone") 
+        )
 
-    #     current_dir = os.path.dirname(os.path.abspath(__file__))
-    #     # set files to use in drag and drop operation
-    #     files = {
-    #         'file1': os.path.join(current_dir, 'doc1Student.docx'),
-    #         'file2': os.path.join(current_dir, 'doc2Student.docx'),
-    #         'file3': os.path.join(current_dir, 'doc2Student.docx')
-    #     }
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        # set files to use in drag and drop operation
+        files = {
+            'file1': os.path.join(current_dir, 'doc1Student.docx'),
+            'file2': os.path.join(current_dir, 'doc2Student.docx'),
+            'file3': os.path.join(current_dir, 'doc2Student.docx')
+        }
         
-    #     # highlight drop area
-    #     self.driver.execute_script("arguments[0].style.border = '2px dashed red';", drop_area)
+        # highlight drop area
+        self.driver.execute_script("arguments[0].style.border = '2px dashed red';", drop_area)
 
-    #     # create actions object
-    #     actions = ActionChains(self.driver)
+        # create actions object
+        actions = ActionChains(self.driver)
 
-    #     # drag and drop files
-    #     for file_name, file_path in files.items():
-    #         actions.drag_and_drop_by_offset(drop_area, 100, 100).perform()
+        # drag and drop files
+        for file_name, file_path in files.items():
+            actions.drag_and_drop_by_offset(drop_area, 100, 100).perform()
 
-    #     # test cases
-    #     # more than max_files
-    #     try:
-    #         WebDriverWait(self.driver, 5).until(EC.alert_is_present())
-    #         alert = self.driver.switch_to.alert
+        # test cases
+        # more than max_files
+        try:
+            WebDriverWait(self.driver, 5).until(EC.alert_is_present())
+            alert = self.driver.switch_to.alert
         
-    #         # verify text
-    #         alert_text = alert.text
-    #         assert "Maximum of 2 files reached" in alert_text
+            # verify text
+            alert_text = alert.text
+            assert "Maximum of 2 files reached" in alert_text
 
-    #         alert.accept()
-    #     except NoAlertPresentException:
-    #         assert False, "Alert for maximum files reached not displayed."
+            alert.accept()
+        except NoAlertPresentException:
+            assert False, "Alert for maximum files reached not displayed."
 
     
 
