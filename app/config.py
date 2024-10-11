7f9a6986c99df14a5339910461e2aa0a107c1b74
Lok Yx <lok@trilliongaming.com>
2024-10-11 13:42:00 +0800

    File Upload Limits Adjustment
    - Update the file upload validation to allow a range between 1 and 9,999 files.
    - If the file count exceeds the range, return the value 999_999_999 as a fallback for exceptional cases.

--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ class Config(object):
     PROJECT_NAME = 'DocuMatcher'
     UPLOAD_EXTENSIONS = ['.docx']
     MAX_CONTENT_LENGTH = 210 * 1024 * 1024  #For additional headers
-    MAX_FILES_UPLOAD = 20
+    MAX_FILES_UPLOAD = -1
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'never-guess'
