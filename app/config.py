8978e1ce8edf21d0ac27560a9cbaa945e7cd88c8
aidan <aidenpower03@gmail.com>
2024-10-11 21:30:43 +0800

    adjusted tests for new code, fix test_max_file_size_limit, manual test config

--- app/config.py
+++ app/config.py
@@ -7,9 +7,10 @@ class Config(object):
     MAX_CONTENT_LENGTH = 210 * 1024 * 1024 #For additional headers
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'never-guess'
 
-class TestingConfig(object):
+class TestingConfig(Config):
     TESTING = True
     DEBUG = True
     PRESERVE_CONTEXT_ON_EXCEPTION = False
     SERVER_NAME = 'localhost:5000'
     MAX_CONTENT_LENGTH = 20 * 1 * 1024 * 1024
+    MAX_FILES_UPLOAD = 20
